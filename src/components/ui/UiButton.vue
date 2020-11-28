92b53b3dda63d80475e50896a11608fa867cf6fb
Natasha Kalacheva <kalachevan615@gmail.com>
2020-11-28 19:02:53 +0700

    Initialized ElementUI, created components/ui, error to compile

---
+++ src/components/ui/UiButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-button v-bind="$attr" v-on="$listeners">
+    <slot/>
+  </el-button>
+</template>
+
+<script>
+export default {
+  name: 'UiButton',
+  props: {
+    msg: String,
+  },
+};
+</script>
---
+++ src/components/ui/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+
+const requireComponent = require.context(
+  './components',
+  false,
+  /Ui[A-Z]\w+\.(vue|js)$/,
+);
+
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(
+
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, ''),
+    ),
+  );
+
+  Vue.component(
+    componentName,
+    componentConfig.default || componentConfig,
+  );
+});
