c5abe0ae753d70a49a700f2b01f4f3b62afa6e67
Natalie <kalachevan615@gmail.com>
2020-12-19 18:03:09 +0700

    Subscriptions page with header & link to Add Subcriptions page

---
+++ src/views/AddSubscription.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page-subscriptions">
+    <div class="page-subscriptions__content">
+      <subscriptions-header />
+    </div>
+  </div>
+</template>
+
+<script>
+import SubscriptionsHeader from '@/components/Subscriptions/SubscriptionsAddHeader'
+
+export default {
+  name: 'Subscriptions',
+  components: {
+    SubscriptionsHeader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-subscriptions {
+  padding: 0 $page-container-padding;
+  box-sizing: border-box;
+  min-height: 100vh;
+  width: 100%;
+
+  &__content {
+    max-width: 768px;
+    margin: 0 auto;
+  }
+}
+</style>
