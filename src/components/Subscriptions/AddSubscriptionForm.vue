00a0b5175b173b97dd53c8081f8adaa7d5da78a6
Natalie <kalachevan615@gmail.com>
2020-12-20 11:12:01 +0700

    Add subscription form

--- src/components/Subscriptions/AddSubscriptionForm.vue
+++ src/components/Subscriptions/AddSubscriptionForm.vue
@@ -10,14 +10,28 @@
       <ui-input v-model="subscriptionForm.name" />
     </ui-form-item>
     <ui-form-item label="Amount" prop="amount">
-      <ui-input v-model.number="subscriptionForm.amount" placeholder="$" type="number"></ui-input>
+      <ui-input v-model.number="subscriptionForm.amount" placeholder="$" type="number" />
     </ui-form-item>
     <ui-form-item label="Next Payment" prop="nextPayment">
-      <ui-input
+      <ui-date-picker
         v-model.number="subscriptionForm.nextPayment"
-        type="text"
+        type="date"
         placeholder="dd/mm/yyyy"
-      ></ui-input>
+      />
+    </ui-form-item>
+    <ui-form-item label="Due Date" prop="dueDate">
+      <ui-date-picker
+        v-model.number="subscriptionForm.dueDate"
+        type="date"
+        placeholder="dd/mm/yyyy"
+      />
+    </ui-form-item>
+    <ui-form-item label="Due Date" prop="dueDate">
+      <ui-select
+        v-model.number="subscriptionForm.billingCycle"
+        type="date"
+        placeholder="dd/mm/yyyy"
+      />
     </ui-form-item>
 
     <ui-button type="primary" size="large" @click.prevent="submitForm"
@@ -39,13 +53,39 @@ export default {
     subscriptionForm: {
       name: '',
       amount: 0,
-      nextPayment: ''
+      nextPayment: '',
+      dueDate: '',
+      billingCycle: [
+        {
+          value: 'Option1',
+          label: 'Option1'
+        },
+        {
+          value: 'Option2',
+          label: 'Option2'
+        },
+        {
+          value: 'Option3',
+          label: 'Option3'
+        },
+        {
+          value: 'Option4',
+          label: 'Option4'
+        },
+        {
+          value: 'Option5',
+          label: 'Option5'
+        }
+      ]
     },
     formRules: {
       name: [{ required: true, message: 'Please input subscription', trigger: 'submit' }],
       amount: [
-        { required: true, message: 'Please input number', trigger: 'blur' },
+        { required: true, message: 'Please input number', trigger: 'submit' },
         { validator: checkNumber, trigger: 'blur' }
+      ],
+      nextPayment: [
+        { required: true, message: 'Please input next payment date', trigger: 'submit' }
       ]
     },
     labelPosition: 'top'
---
+++ src/components/ui/UiDatePicker.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-date-picker class="ui-date-picker" v-bind="$attrs" v-on="$listeners" :style="inputStyles" />
+</template>
+
+<script>
+export default {
+  name: 'UiDatePicker',
+  props: {
+    width: {
+      type: [Number, String],
+      default: '250px'
+    },
+    isFullWidth: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    inputStyles() {
+      return {
+        width: this.isFullWidth ? '100%' : this.width
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ui-date-picker /deep/ {
+  margin: 0;
+  width: auto;
+  ::placeholder {
+    color: $color-text-grey;
+  }
+  .el-input__inner {
+    position: relative;
+    height: 50px;
+    padding-left: 25px;
+    color: $color-dark-blue;
+    background: $color-light-grey;
+    border-color: $color-light-grey;
+    border-radius: 18px;
+
+    &:focus,
+    &:hover {
+      border-color: $color-blue;
+    }
+  }
+  .el-input__clear,
+  .el-icon-date {
+    color: $color-blue;
+  }
+  .el-input__prefix {
+    left: auto;
+    right: 5px;
+  }
+}
+
+.el-date-picker {
+  font-family: Arial, sans-serif;
+}
+</style>
