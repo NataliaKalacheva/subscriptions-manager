4f893ed37699ef03f402ccb428336d1b772d47be
Natalie <kalachevan615@gmail.com>
2020-12-30 08:29:10 +0700

    Subscription icon

--- src/components/Subscriptions/SubscriptionHeader.vue
+++ src/components/Subscriptions/SubscriptionHeader.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="section-header">
+  <div class="subscription-header">
     <ui-icon-bubble />
-    <h1 class="section-header__title">{{ title }}</h1>
+    <subscription-icon :title="title" />
+    <h1 class="subscription-header__title">{{ title }}</h1>
     <slot />
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import SubscriptionIcon from '@/components/Subscriptions/SubscriptionIcon'
 
 export default {
   name: 'SubscriptionHeader',
+  components: {
+    SubscriptionIcon
+  },
   props: {
     title: {
       type: String,
@@ -22,3 +27,20 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.subscription-header {
+  margin: 20px 0 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+
+  &__title {
+    margin: 0 0;
+    font-size: 26px;
+    letter-spacing: -0.98;
+  }
+}
+</style>
---
+++ src/components/Subscriptions/SubscriptionIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="subscription-icon">
+    <img :src="icon" :alt="title" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubscriptionHeader',
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.subscription-icon {
+  width: 72px;
+  height: 72px;
+  border-radius: 5px;
+  background: $color-grey;
+}
+</style>
