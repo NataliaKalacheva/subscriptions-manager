18547466b837d4979568665631ea5e2f42a554df
Natalie <kalachevan615@gmail.com>
2021-02-11 19:48:42 +0700

    call submitForm method, when submit form

--- src/components/Auth/__tests__/loginForm.spec.js
+++ src/components/Auth/__tests__/loginForm.spec.js
@@ -1,12 +1,72 @@
-import { mount } from '@vue/test-utils'
+import { mount, RouterLinkStub, createLocalVue, config } from '@vue/test-utils'
 import LoginForm from '@/components/Auth/LoginForm.vue'
+import Vuex from 'vuex'
+import sinon from 'sinon'
+
+config.showDeprecationWarnings = false
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
 
 describe('LoginForm', () => {
-  const wrapper = mount(LoginForm)
+  let actions
+  let store
+  let methods
+
+  beforeEach(() => {
+    actions = {
+      login: jest.fn()
+    }
+    store = new Vuex.Store({
+      actions
+    })
+    methods = {
+      submitForm: jest.fn(),
+      clearValidate: jest.fn()
+    }
+  })
 
   it('renders the correct markup', () => {
-    expect(wrapper.contains('.login-form')).toBe(true)
-    expect(wrapper.contains('.login-submit')).toBe(true)
+    const wrapper = mount(LoginForm)
+    const loginForm = wrapper.findComponent(LoginForm)
+    const loginSubmit = wrapper.find('.login-submit')
+    expect(loginForm.exists()).toBe(true)
+    expect(loginSubmit.exists()).toBe(true)
   })
+
+  it('contains reset link', () => {
+    const wrapper = mount(LoginForm, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+    const resetLink = wrapper.find(RouterLinkStub)
+    expect(resetLink.props().to.name).toBe('ResetPassword')
+  })
+
+  it('call "submitForm" method, when submit form"', () => {
+    const wrapper = mount(LoginForm)
+    const submitBtn = wrapper.find('.login-submit')
+    const submitFormStub = sinon.stub()
+
+    wrapper.setMethods({ submitForm: submitFormStub })
+    submitBtn.trigger('click')
+    expect(submitFormStub.called).toBe(true)
+  })
+
+  // it('call "clearValidate" method, when focus on input"', () => {
+  //   const wrapper = mount(LoginForm)
+  //   const clearValidateStub = sinon.stub()
+  //   const input = wrapper.find('.ui-input')
+
+  //   wrapper.setMethods({ clearValidate: clearValidateStub })
+  //   input.trigger('focus')
+  //   expect(clearValidateStub.called).toBe(true)
+  // })
+
+  // it('call "login" action, when submit valid form"', () => {
+  //   const wrapper = mount(LoginForm, { store, localVue })
+  // })
 
 })
